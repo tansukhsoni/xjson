a9e3a62208356c3f83d6769395be92d50709f4c7
Chitresh Kakwani <chitreshkakwani@gmail.com>
2013-01-05 20:17:50 +0530

    JSON Tokenizer tests added.

--- src/test/java/com/jsonutils/xjson/JSONTokenizerTest.java
+++ src/test/java/com/jsonutils/xjson/JSONTokenizerTest.java
@@ -4,18 +4,13 @@ import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-    extends TestCase
-{
-    /**
+public class JSONTokenizerTest extends TestCase {
+	/**
      * Create the test case
      *
      * @param testName name of the test case
      */
-    public AppTest( String testName )
+    public JSONTokenizerTest( String testName )
     {
         super( testName );
     }
@@ -25,7 +20,7 @@ public class AppTest
      */
     public static Test suite()
     {
-        return new TestSuite( AppTest.class );
+        return new TestSuite( JSONTokenizerTest.class );
     }
 
     /**
