82f5184465601373cd24cf9514102779db1197ed
Chitresh Kakwani <chitreshkakwani@gmail.com>
2013-01-05 20:25:08 +0530

    JSONTokenizer test cases added.

--- src/test/java/com/jsonutils/xjson/JSONTokenizerTest.java
+++ src/test/java/com/jsonutils/xjson/JSONTokenizerTest.java
@@ -22,12 +22,59 @@ public class JSONTokenizerTest extends TestCase {
     {
         return new TestSuite( JSONTokenizerTest.class );
     }
+    
+    public void testApp(){
+    	tokenizerTest1();
+    	tokenizerTest2();
+    	tokenizerTest3();
+    	tokenizerTest4();
+    }
 
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
+    public void tokenizerTest1(){
+    	JSONTokenizer tokenizer = new JSONTokenizer("{\"key\":\"value\"}");
+    	assertEquals(JSONTokenizer.Token.START_BRACE, tokenizer.currentToken());
+    	assertEquals(JSONTokenizer.Token.COLON, tokenizer.nextToken());
+    	assertEquals("\"key\"", tokenizer.getCurrent());
+    	assertEquals(JSONTokenizer.Token.COLON, tokenizer.currentToken());
+    	assertEquals(JSONTokenizer.Token.END_BRACE, tokenizer.nextToken());
+    	assertEquals("\"value\"", tokenizer.getCurrent());
+    	assertEquals(JSONTokenizer.Token.END_BRACE, tokenizer.currentToken());
+    	assertEquals(JSONTokenizer.Token.EOF, tokenizer.nextToken());
+    }
+    
+    public void tokenizerTest2(){
+    	JSONTokenizer tokenizer = new JSONTokenizer("{}");
+    	assertEquals(JSONTokenizer.Token.START_BRACE, tokenizer.currentToken());
+    	assertEquals(JSONTokenizer.Token.END_BRACE, tokenizer.nextToken());
+    	assertEquals("", tokenizer.getCurrent());
+    	assertEquals(JSONTokenizer.Token.END_BRACE, tokenizer.currentToken());
+    	assertEquals(JSONTokenizer.Token.EOF, tokenizer.nextToken());
+    }
+    
+    public void tokenizerTest3(){
+    	JSONTokenizer tokenizer = new JSONTokenizer("[\"key1\":\"value1\",\"key2\":\"value2\"]");
+    	assertEquals(JSONTokenizer.Token.START_BRACKET, tokenizer.currentToken());
+    	assertEquals(JSONTokenizer.Token.COLON, tokenizer.nextToken());
+    	assertEquals("\"key1\"", tokenizer.getCurrent());
+    	assertEquals(JSONTokenizer.Token.COLON, tokenizer.currentToken());
+    	assertEquals(JSONTokenizer.Token.COMMA, tokenizer.nextToken());
+    	assertEquals("\"value1\"", tokenizer.getCurrent());
+    	assertEquals(JSONTokenizer.Token.COMMA, tokenizer.currentToken());
+    	assertEquals(JSONTokenizer.Token.COLON, tokenizer.nextToken());
+    	assertEquals("\"key2\"", tokenizer.getCurrent());
+    	assertEquals(JSONTokenizer.Token.COLON, tokenizer.currentToken());
+    	assertEquals(JSONTokenizer.Token.END_BRACKET, tokenizer.nextToken());
+    	assertEquals("\"value2\"", tokenizer.getCurrent());
+    	assertEquals(JSONTokenizer.Token.END_BRACKET, tokenizer.currentToken());
+    	assertEquals(JSONTokenizer.Token.EOF, tokenizer.nextToken());
+    }
+    
+    public void tokenizerTest4(){
+    	JSONTokenizer tokenizer = new JSONTokenizer("[]");
+    	assertEquals(JSONTokenizer.Token.START_BRACKET, tokenizer.currentToken());
+    	assertEquals(JSONTokenizer.Token.END_BRACKET, tokenizer.nextToken());
+    	assertEquals("", tokenizer.getCurrent());
+    	assertEquals(JSONTokenizer.Token.END_BRACKET, tokenizer.currentToken());
+    	assertEquals(JSONTokenizer.Token.EOF, tokenizer.nextToken());
     }
 }
